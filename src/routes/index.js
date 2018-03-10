bff2afe61dc648c3ef18b9274c378f7f468f7ab3
Tijmen de Graaff <tijmen76@gmail.com>
2018-03-10 19:01:30 +0100

    add register form with mutation

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Home from './Home'
+import Register from './Register'
 import NotFoundPage from './NotFoundPage'
 
 export default () => (
     <BrowserRouter>
         <Switch>
             <Route path="/" exact component={Home} />
+            <Route path="/register" exact component={Register} />
             <Route component={NotFoundPage} />
         </Switch>
     </BrowserRouter>
