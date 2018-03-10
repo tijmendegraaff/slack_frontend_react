6a15c9ab2698214af13472bf9255bfe7b01117ab
Tijmen de Graaff <tijmen76@gmail.com>
2018-03-10 15:14:50 +0100

    setup apollo

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
 import 'normalize.css/normalize.css';
-import './styles/styles.scss';
 
-ReactDOM.render(<div>Hello</div>, document.getElementById('app'));
+import './styles/styles.scss';
+import client from './apollo';
+
+const App = (
+    <ApolloProvider client={client}>
+      <div>hello</div>
+    </ApolloProvider>
+  );
+
+ReactDOM.render(App, document.getElementById('app'));
