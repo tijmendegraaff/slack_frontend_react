783d74350c5bb15186a127d9f79843e23317c291
Tijmen de Graaff <tijmen76@gmail.com>
2018-03-11 16:02:05 +0100

    add loginPage to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import Home from './Home'
-import Register from './Register'
+import HomePage from './HomePage'
+import RegisterPage from './RegisterPage'
+import LoginPage from './LoginPage'
 import NotFoundPage from './NotFoundPage'
 
 export default () => (
     <BrowserRouter>
         <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/register" exact component={Register} />
+            <Route path="/" exact component={HomePage} />
+            <Route path="/register" exact component={RegisterPage} />
+            <Route path="/login" exact component={LoginPage} />
             <Route component={NotFoundPage} />
         </Switch>
     </BrowserRouter>
