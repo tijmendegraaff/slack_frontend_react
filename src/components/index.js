b667a17d15fefcf302652e7e8c267e574e3d2c3d
Tijmen de Graaff <tijmen76@gmail.com>
2018-03-18 01:35:30 +0100

    import all components to index file

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import AppWrapper from './AppWrapper'
+import Channels from './Channels'
+import Header from './Header'
+import Teams from './Teams'
+import MessageInput from './MessageInput'
+
+export { AppWrapper, Channels, Header, Teams, MessageInput }
