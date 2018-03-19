b9c68503940796152fae2576bfb32a2da23dd971
Tijmen de Graaff <tijmen76@gmail.com>
2018-03-19 17:03:43 +0100

    setup Team component to use props

--- src/components/Channels.js
+++ src/components/Channels.js
@@ -1,10 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Channels = props => <div className="channels">{props.children}</div>
+const channel = ({ id, name }) => <li key={`channel-${id}`}>{name}</li>
 
-Channels.propTypes = {
-    children: PropTypes.any.isRequired
-}
+const user = ({ id, name }) => <li key={`user-${id}`}>{name}</li>
+
+const Channels = ({
+    teamName, username, channels, users
+}) => (
+    <div className="channels">
+        <div>
+            {teamName}
+            {username}
+        </div>
+        <div>
+            <ul>
+                <li>Channels</li>
+                {channels.map(channel)}
+            </ul>
+        </div>
+        <div>
+            <ul>
+                <li>Users</li>
+                {users.map(user)}
+            </ul>
+        </div>
+    </div>
+)
+
+// Channels.propTypes = {
+//     children: PropTypes.any.isRequired
+// }
 
 export default Channels
