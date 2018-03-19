c22b7e97e588c9ce5faeb3aed4884a17b79a0135
Tijmen de Graaff <tijmen76@gmail.com>
2018-03-19 17:04:11 +0100

    add Message input

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Teams = props => <div className="teams">{props.children}</div>
+const team = ({ id, name }) => (
+    <li key={`team-${id}`} className="team-list-item">
+        {name.charAt(0).toUpperCase()}
+    </li>
+)
+
+const Teams = ({ teams }) => (
+    <div className="teams">
+        <ul className="team-list">{teams.map(team)}</ul>
+    </div>
+)
 
 Teams.propTypes = {
-    children: PropTypes.any.isRequired
+    teams: PropTypes.array.isRequired
 }
 
 export default Teams
