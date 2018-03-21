7ab57307aebdf676f078fe8c21d9f643b048d9df
Tijmen de Graaff <tijmen76@gmail.com>
2018-03-21 15:38:03 +0100

    add create channel mutation

---
+++ src/graphql/mutations/createChannelMutation.js
@@ -0,0 +1,11 @@
+import gql from 'graphql-tag'
+
+export default gql`
+    mutation createChannel($input: ChannelInputType!) {
+        createChannel(input: $input) {
+            id
+            name
+            isPublic
+        }
+    }
+`
