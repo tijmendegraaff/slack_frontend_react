f4754ee1b52726719aea016b1fd933562834bf22
Tijmen de Graaff <tijmen76@gmail.com>
2018-03-24 00:02:46 +0100

    import direct message container

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,4 +1,5 @@
 import MessageContainer from './MessageContainer'
 import SideBarContainer from './SideBarContainer'
+import DirectMessageContainer from './DirectMessageContainer'
 
-export { MessageContainer, SideBarContainer }
+export { MessageContainer, SideBarContainer, DirectMessageContainer }
