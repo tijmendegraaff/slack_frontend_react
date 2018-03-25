2aa943832b3ca86ff607572701e1ee644340df19
Tijmen de Graaff <tijmen76@gmail.com>
2018-03-25 08:11:42 +0200

    create multiselect component

---
+++ src/components/MultiSelectUserInput.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Dropdown } from 'semantic-ui-react'
+
+const MultiSelectUserInput = ({
+    selectedMembers,
+    handleChange,
+    placeholder,
+    members,
+    currentUserId
+}) => {
+    console.log(members
+        .filter(member => member.id !== currentUserId)
+        .map(member => ({ key: member.userName, value: member.id, text: member.userName })))
+    return (
+        <Dropdown
+            value={selectedMembers}
+            onChange={(e, { value }) => handleChange(e, { value })}
+            placeholder={placeholder}
+            fluid
+            multiple
+            search
+            selection
+            options={members
+                .filter(member => member.id !== currentUserId)
+                .map(member => ({ key: member.userName, value: member.id, text: member.userName }))}
+        />
+    )
+}
+
+MultiSelectUserInput.propTypes = {
+    selectedMembers: PropTypes.array.isRequired,
+    handleChange: PropTypes.func.isRequired,
+    placeholder: PropTypes.string.isRequired,
+    members: PropTypes.array.isRequired,
+    currentUserId: PropTypes.string.isRequired
+}
+
+export default MultiSelectUserInput
