08d013b4bb3783e05048eddf55d731c9f8aa740b
Tijmen de Graaff <tijmen76@gmail.com>
2018-03-27 19:21:33 +0200

    exclude userinformation

--- src/graphql/mutations/createSessionMutation.js
+++ src/graphql/mutations/createSessionMutation.js
@@ -4,18 +4,6 @@ export default gql`
     mutation createSession($input: SessionInputType!) {
         createSession(input: $input) {
             token
-            user {
-                id
-                lastName
-                teams {
-                    id
-                    name
-                    channels {
-                        id
-                        name
-                    }
-                }
-            }
         }
     }
 `
